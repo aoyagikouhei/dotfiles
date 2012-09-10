a81aaae7050605842e58b14d0e653017ebfaf0d7
aoyagikouhei <aoyagi.kouhei@gmail.com>
2012-09-10 21:27:47 +0900

    modify mountain lion

---
+++ bin/install_mac.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ln -s /Users/aoyagikouhei/Documents/git/dotfiles/dot.bashrc /Users/aoyagikouhei/.bashrc
+ln -s /Users/aoyagikouhei/Documents/git/dotfiles/dot.bash_profile /Users/aoyagikouhei/.bash_profile
+ln -s /Users/aoyagikouhei/Documents/git/dotfiles/dot.gemrc /Users/aoyagikouhei/.gemrc
+ln -s /Users/aoyagikouhei/Documents/git/dotfiles/dot.gitconfig /Users/aoyagikouhei/.gitconfig
+ln -s /Users/aoyagikouhei/Documents/git/dotfiles/dot.gitignore /Users/aoyagikouhei/.gitignore
+ln -s /Users/aoyagikouhei/Documents/git/dotfiles/dot.bash_aliases_mac /Users/aoyagikouhei/.bash_aliases
+ln -s /Users/aoyagikouhei/Documents/git/dotfiles/dot.vimrc /Users/aoyagikouhei/.vimrc
+ln -s /Users/aoyagikouhei/Documents/git/dotfiles/dot.vim /Users/aoyagikouhei/.vim
--- dot.bashrc
+++ dot.bashrc
@@ -17,14 +17,8 @@ if [ "${OSTYPE}" = "linux-gnu" ]; then
   export RUBY_HOME=/opt/ruby
   export JAVA_HOME=/usr/lib64/jvm/java-6-openjdk
   export PATH=$RUBY_HOME/bin:$PHP_HOME/bin:$GAUCHE_HOME/bin:$GIT_HOME/bin:$PATH
-elif [ "${OSTYPE}" = "darwin10.0" ]; then
-  export GIT_HOME=~/opt/git
-  export GAUCHE_HOME=~/opt/Gauche
-  export RUBY_HOME=~/opt/ruby
-  export JRUBY_HOME=~/opt/jruby
-  export JRUBY_OPTS=--1.9
-  export PG_HOME=~/opt/postgresql
+elif [ "${OSTYPE}" = "darwin12.1.0" ]; then
   export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
-  export PATH=~/bin:$GAUCHE_HOME/bin:$PG_HOME/bin:$RUBY_HOME/bin:$GIT_HOME/bin:$JRUBY_HOME/bin:$PATH
+  export PATH=~/bin:$PATH
 fi
 
